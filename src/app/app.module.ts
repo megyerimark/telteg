4a73197f1ca7e7eb9ca357a9daa5ad0a5623fd45
Megyeri Márk Máté <megyko1234@gmail.com>
2022-11-28 09:20:46 +0100

    Komponencek letöltése

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalcComponent } from './calc/calc.component';
+import { CalComponent } from './cal/cal.component';
+import { AboutComponent } from './about/about.component';
+import { GitComponent } from './git/git.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcComponent,
+    CalComponent,
+    AboutComponent,
+    GitComponent
   ],
   imports: [
     BrowserModule,
