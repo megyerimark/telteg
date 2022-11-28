20fe7ed439485499d059459a88387a876e67b5c8
Megyeri Márk Máté <megyko1234@gmail.com>
2022-11-28 09:26:22 +0100

    calcComponent kész

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CalcComponent } from './calc/calc.component';
+import { GitComponent } from './git/git.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "calc", component:CalcComponent},
+  {path: "about", component:AboutComponent},
+  {path: "git", component:GitComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -7,6 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalcComponent implements OnInit {
 
+  title = 'Télteg';
+  aSide !:number;
+  bSide !:number;
+  terulet !:number;
+  areaShow = false;
+  
+
+  calcArea():void{
+    if (this.checValidation()){
+      console.log('Működik');
+      this.terulet = 2*(this.aSide*this.bSide)
+      this.aSide = 0;
+      this.bSide = 0;
+      this.areaShow = true
+    }else{
+     alert ("Írj be egy számot ami nagyobb , mint 0 vagy EGYES!")
+    }
+  }
+
+
+
+  checValidation():boolean{
+    let valid = true;
+    if(this.aSide <= 0){
+      valid = false ;
+    }
+    if(this.bSide <= 0){
+      valid = false
+    }
+    return valid;
+  }
+  hideArea():void{
+    this.areaShow = false;
+  }
+  Area():void{
+    this.areaShow = false;
+  }
+
+
+
+
   constructor() { }
 
   ngOnInit(): void {
